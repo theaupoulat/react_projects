da27c3cbeacf37026fe4323ce0173b75fb4850ef
Theau Poulat <theau.poulat@gmail.com>
2021-10-29 00:52:08 +0200

    fix: style prop

--- my-bank/src/Account.js
+++ my-bank/src/Account.js
@@ -1,24 +1,26 @@
-import React from "react"
+import React from "react";
 
-import Line from './Operation'
-import Button from './Button'
+import Line from "./Operation";
+import Button from "./Button";
 
 class Account extends React.Component {
-    render = () => {
-        return (
-            <div>
-                <div className="Account--header" style={this.props.data.color}>
-                    <h2> {this.props.data.name}</h2>
-                    <h2> {this.props.data.balance} €</h2>
-                </div>
-                <div className="Account--lines">
-                    <Line operations={this.props.data.operations} />
-                    <Button>SEE MORE</Button>
-                </div>
-            </div>
-
-        )
-    }
+  render = () => {
+    return (
+      <div>
+        <div
+          className="Account--header"
+          style={{ color: this.props.data.color }}
+        >
+          <h2> {this.props.data.name}</h2>
+          <h2> {this.props.data.balance} €</h2>
+        </div>
+        <div className="Account--lines">
+          <Line operations={this.props.data.operations} />
+          <Button>SEE MORE</Button>
+        </div>
+      </div>
+    );
+  };
 }
 
-export default Account
+export default Account;
